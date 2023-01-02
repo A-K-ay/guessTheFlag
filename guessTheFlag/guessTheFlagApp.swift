f6f5cf602243e23a528a05de99b7cf07941dfdb1
Abhimanyu Kharwal <1abhimanyukharwal.ak@gmail.com>
2023-01-02 22:15:19 +0530

    Initial Commit

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by Abhimanyu Personal on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
